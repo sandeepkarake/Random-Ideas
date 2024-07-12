4ff25a0bdb1a14bd5a314acf962b2534541fa666
Rutuja Jain <sandeepkarake.126@gmail.com>
2024-07-12 14:46:55 +0530

    second commit

---
+++ routes/ideas.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const router = express.Router();
+const Idea = require('../models/Idea')
+
+// Get all Ideas
+router.get('/', async (req, res) => {
+
+    try {
+        const ideas = await Idea.find();
+        res.send({ success: true, data: ideas })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            success: false,
+            error: 'Something went wrong'
+        })
+    }
+})
+// Get single Idea
+router.get('/:id', async (req, res) => {
+    try {
+        const idea = await Idea.findById(req.params.id);
+        res.send({ success: true, data: idea })
+    } catch (error) {
+
+        if (!idea) {
+            return res.status(404).json({
+                success: false,
+                error: 'Resource Not found'
+            })
+        }
+    };
+
+})
+// Add an Idea
+router.post('/', async (req, res) => {
+    const idea = new Idea({
+        text: req.body.text,
+        tag: req.body.tag,
+        username: req.body.username,
+    });
+
+    try {
+        const savedIDea = await idea.save();
+        res.json({
+            success: true,
+            data: savedIDea
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            success: false,
+            error: 'Something went wrong'
+        })
+    }
+
+})
+// Update an Idea
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedIdea = await Idea.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: {
+                    text: req.body.text,
+                    tag: req.body.tag
+                }
+            },
+            {
+                new: true
+            }
+        )
+        res.json({ success: true, data: updatedIdea })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, error: 'Something went wrong' })
+    }
+})
+// Delete an Idea
+router.delete('/:id', async (req, res) => {
+    try {
+        await Idea.findByIdAndDelete(req.params.id)
+        res.send({ success: true, data: {} })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, error: 'Something went wrong' })
+    }
+})
+module.exports = router;
--- server.js
+++ server.js
@@ -1,52 +1,22 @@
 const express = require('express');
-const port = 5000;
+require('dotenv').config();
+const port = process.env.PORT;
+const connectDB = require('./config/db')
 
-const ideas = [
-    {
-        id : 1,
-        text : 'Positive NewsLetter. a newsletter that only shares positive, uplifiting news',
-        tag : 'Technology',
-        username : 'TonyStark',
-        date : '2022-01-02',
-    },
-    {
-        id : 2,
-        text : 'Milk cartons that turn a different color the older that your milk is getting',
-        tag : 'Invention',
-        username : 'SteveRogers',
-        date : '2022-01-02',
-    },
-    {
-        id : 1,
-        text : 'ATM location app which lets you know where the closet ATM is and id it is in service',
-        tag : 'Software',
-        username : 'BruceBanner',
-        date : '2022-01-02',
-    }
-]
-
+connectDB();
 const app = express();
 
-app.get('/' , (req, res) => {
-    res.send({message : 'Welcome to the RandomIdeas API'})
-})
-// Get all Ideas
-app.get('/api/ideas' , (req, res) => {
-    res.send({ success : true, data : ideas})
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}))
+app.get('/', (req, res) => {
+    res.send({ message: 'Welcome to the RandomIdeas API' })
 })
 
-app.get('/api/ideas/:id' , (req, res) => {
-    const idea = ideas.find((idea) => idea.id === +req.params.id)
-
-    if (!idea) {
-        return res.status(404).json({
-            success : false,
-            error : 'Resource Not found'
-        })
-    };
-
-    res.send({ success : true, data : idea})
-})
+const ideasrouter = require('./routes/ideas')
+app.use('/api/ideas', ideasrouter)
 
 app.listen(port, () => {
     console.log(`Server listening at port ${port}`)
